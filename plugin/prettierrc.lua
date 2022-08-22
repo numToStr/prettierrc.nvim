6a6da38b292031b28645e5836b3d315e3334aa59
numToStr <vikasraj1911@gmail.com>
2022-08-22 18:03:40 +0530

    change: lazily apply settings

--- plugin/prettierrc.lua
+++ plugin/prettierrc.lua
@@ -1,17 +1,23 @@
+local rc = require('prettierrc')
+
 local augroup = vim.api.nvim_create_augroup('prettierrc', { clear = true })
 
 vim.api.nvim_create_autocmd('VimEnter', {
     group = augroup,
     callback = function()
-        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-            require('prettierrc').init(buf)
-        end
+        vim.schedule(function()
+            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                rc.init(buf)
+            end
+        end)
     end,
 })
 
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead', 'BufFilePost' }, {
     group = augroup,
     callback = function(data)
-        require('prettierrc').init(data.buf)
+        vim.schedule(function()
+            rc.init(data.buf)
+        end)
     end,
 })
